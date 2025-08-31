c3a381af3fb9eb9cdaeb498d149b7a35bb66163f
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-08-31 17:27:53 -0300

    Ajustes no app de contas

--- contas/urls.py
+++ contas/urls.py
@@ -7,7 +7,7 @@ app_name = "contas"
 
 urlpatterns = [
     path("login/", RememberLoginView.as_view(template_name="login.html"), name="login"),
-    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    path('logout/',auth_view.LogoutView.as_view(next_page=reverse_lazy('contas:login')), name="logout"),
     path('editar_perfil/<int:pk>', EditarPerfil.as_view(), name="editar_perfil"),
     path('criar_conta/', CriarConta.as_view(), name="criar_conta"),
     path('redefinir_senha/', auth_view.PasswordChangeView.as_view(template_name='editarperfil.html', success_url=reverse_lazy('livros:index')), name="redefinir_senha"),
